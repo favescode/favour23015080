c16966d924de2106da97bd7e02e8007ce0d3e571
favescode <Favouranietie308@gmail.com>
2024-06-12 14:12:58 +0100

    copyfile.py

--- Regex/copyfile.py
+++ Regex/copyfile.py
@@ -1,7 +1,7 @@
-source = input("Enter name of the source file: ")
-with open("text.txt","r")as file:
-    content = file.read()
-    destination = input("Enter the name of the destination file: ")
-    with open(destination,"w")as file:
-        copied = file.write(content)
+file = input("Enter name of the source file: ")
+with open("new_text.txt","r")as file:
+    contain = file.read()
+    copy = input("Enter the name of the destination file: ")
+    with open(copy,"w")as file:
+        copied = file.write(contain)
         print("copied Successful ")
